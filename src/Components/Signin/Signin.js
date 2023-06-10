4685520e2d455f75441e17fe61ef9f875f41050c
Ahmad Pathan <pathanahmad2334@gmail.com>
2023-06-10 13:13:17 +0530

    Fixed the address of fetch link in signin.js

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://face-detection-website-by-pathan-ahmad-y8c4.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
